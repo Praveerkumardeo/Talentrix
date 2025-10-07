17821dc9eb1cea4b2883b01fecf85948a22ad0a5
Praveerkumardeo <praveerkumardeo@gmail.com>
2025-10-07 14:45:54 +0530

    pass encoder func added

--- Talentix/src/main/java/com/project/Talentix/util/PassEncoder.java
+++ Talentix/src/main/java/com/project/Talentix/util/PassEncoder.java
@@ -1,5 +1,17 @@
 package com.project.Talentix.util;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 public class PassEncoder {
-	
+    // Encode (hash) a plain password
+    public static String encode(String plainPassword) {
+        return BCrypt.withDefaults().hashToString(12, plainPassword.toCharArray());
+        // 12 is the cost factor (strength), can be adjusted
+    }
+
+    // Verify a password against a hashed password
+    public static boolean verify(String plainPassword, String hashedPassword) {
+        BCrypt.Result result = BCrypt.verifyer().verify(plainPassword.toCharArray(), hashedPassword);
+        return result.verified;
+    }
 }
