6c09dacff3b497c50e2ff35ae0936bfb340ad892
Praveerkumardeo <praveerkumardeo@gmail.com>
2025-10-07 14:17:17 +0530

    rest controller

--- Talentix/src/main/java/com/project/Talentix/register/RegisterController.java
+++ Talentix/src/main/java/com/project/Talentix/register/RegisterController.java
@@ -1,8 +1,17 @@
 package com.project.Talentix.register;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.Talentix.models.User;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/")
 public class RegisterController {
-
+	@GetMapping("register")
+	public String registerUser() {
+		// Registration logic here
+		return "User registered successfully!";
+	}
 }
